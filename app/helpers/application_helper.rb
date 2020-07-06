7a0446148d3f74d97abadbb01c097afb3cfc26a0
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-07-06 17:48:04 +0000

    Automatically backed up by Learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,6 @@ module ApplicationHelper
     content_for :title, text
   end
   
+  #We use the Rails content_for helper within our custom helper here. Helpers on helpers on helpers! What this will do is send our text to the place in our application layout that is expecting some content for the :title.
+  
 end
